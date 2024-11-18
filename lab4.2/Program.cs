6204c6c5078d896d950dbabc7ade6ca360c4b17c
user <user@DESKTOP-DV6KRKO>
2024-11-18 21:33:38 +0200

    Добавьте файлы проекта.

---
+++ lab4.2/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+
+
+int[,] A = new int[5, 5]
+        {
+            {0, 0, 1, 0, 0},
+            {0, 0, 1, 0, 0},
+            {1, 1, 1, 1, 1},
+            {0, 0, 1, 0, 0},
+            {0, 0, 1, 0, 0}
+        };
+
+
+for (int i = 0; i < A.GetLength(0); i++)
+{
+    for (int j = 0; j < A.GetLength(1); j++)
+    {
+        Console.Write(A[i, j] + " ");
+    }
+    Console.WriteLine(A);
+}
